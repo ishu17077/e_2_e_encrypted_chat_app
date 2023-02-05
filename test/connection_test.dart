a61badd1dc0e26f895e49ca0d13bb6172b3b1238
ishukumar5663 <ishukumar5663@users.noreply.github.com>
2023-02-05 22:08:20 +0530

    Bhagwan aapki leela aparamprar ha...Mera commit swikar kare :)

---
+++ test/connection_test.dart
@@ -0,0 +1,41 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+// import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+// import 'package:e_2_e_encrypted_chat_app/main.dart';
+
+void main() async {
+  //? Maybe this is not a bug, but a feature
+
+  test('Checking for internet connection', () async {
+    http.Response response = await http.get(
+      Uri.parse('https://www.google.com'),
+      headers: {"Accept": "application/json"},
+    );
+    print("\\\\x1B[32mInternet is phenomenal ;)");
+    expectLater(response.statusCode, 200);
+  });
+
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const MyApp());
+
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+}
